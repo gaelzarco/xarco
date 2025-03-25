74cdc4c53cf3cf19b283dd346fdf6d842dda49cd
Gael Zarco <gaelxarco@icloud.com>
2025-03-25 13:39:31 -0700

    optimizations and styling

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ struct Email {
     body: String,
 }
 
-fn json_body() -> impl Filter<Extract = (Email,), Error = warp::Rejection> + Clone {
+fn json_body() -> impl Filter<
+Extract = (Email,), Error = warp::Rejection
+> + Clone {
     // When accepting a body, we want a JSON body
     // (and to reject huge payloads)...
     warp::body::content_length_limit(1024 * 16).and(warp::body::json())
