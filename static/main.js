c6aa95839b2793bc59e9bebb9a7e47d6ab870396
Gael Zarco <gaelxarco@icloud.com>
2025-03-26 17:21:04 -0700

    progress

--- static/main.js
+++ static/main.js
@@ -2,6 +2,12 @@
  * /static/main.js
 */
 
+import * as THREE from 'three';
+
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('body').style.opacity = 1
 })
+
+const scene=new THREE.Scene()
+
+console.log(scene);
